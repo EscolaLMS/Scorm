056f51ca0679f8119f16e9410f619ac7b50d2f84
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 16:39:03 +0100

    Add translations (#39)

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,13 @@
+<?php
+
+use EscolaLms\Scorm\Enums\ScormPermissionsEnum;
+
+return [
+    ScormPermissionsEnum::SCORM_LIST => 'Lista scorm',
+    ScormPermissionsEnum::SCORM_READ => 'Wyświetl scorm',
+    ScormPermissionsEnum::SCORM_CREATE => 'Utwórz scorm',
+    ScormPermissionsEnum::SCORM_DELETE => 'Usuń scorm',
+    ScormPermissionsEnum::SCORM_UPDATE => 'Aktualizuj scorm',
+    ScormPermissionsEnum::SCORM_GET_TRACK => 'Pobierz postęp scorm',
+    ScormPermissionsEnum::SCORM_SET_TRACK => 'Ustaw postęp scorm',
+];
